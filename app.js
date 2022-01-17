4e0e4a9e70a9e2f09b4ef434ba003c947697662e
trinh172 <tuyettrinhaquarius172@gmail.com>
2022-01-18 01:37:24 +0700

    fix bug cors

--- app.js
+++ app.js
@@ -47,7 +47,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   credentials: true, 
 //   origin: DOMAIN_FE.substring(0, DOMAIN_FE.length - 1)
 // }));
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.use('/', authen_author);
 app.use(AuthMiddleWare.isAuthor);
